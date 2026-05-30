0eddb7ecebc5e71593c5629c65f3f2960f921c9a
Junaid Hussain <134702433+JunazzSk@users.noreply.github.com>
2026-05-31 02:08:44 +0530

    Update SonarQube version and Java installation

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 cd /opt/
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
-unzip sonarqube-8.9.6.50800.zip
-sudo dnf install java-17-amazon-corretto -y
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-26.3.0.120487.zip
+unzip sonarqube-26.3.0.120487.zip
+sudo dnf install java-21-amazon-corretto -y
 useradd sonar
-chown sonar:sonar sonarqube-8.9.6.50800 -R
-chmod 777 sonarqube-8.9.6.50800 -R
+chown sonar:sonar sonarqube-26.3.0.120487 -R
+chmod 777 sonarqube-26.3.0.120487 -R
 su - sonar
 # use the below command manually after installation
-#sh /opt/sonarqube-8.9.6.50800/bin/linux-x86-64/sonar.sh start
+#sh /opt/sonarqube-26.3.0.120487/bin/linux-x86-64/sonar.sh start
 #echo "user=admin & password=admin"
